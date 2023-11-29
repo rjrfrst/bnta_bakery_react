777b2e5685b81f5b2e12b074f3682a0b9b9bfdc9
Rayster Ramos <rjrfrst@gmail.com>
2023-11-29 16:46:40 +0000

    added state in the container

---
+++ src/containers/cakeContainer.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const CakeContainer = () => {
+
+    // Containers should have states therefore: 
+    const [victoriaSpongeCount, setvictoriaSpongeCount] = useState(0);
+    const [teaLoafCount, setTeaLoafCount] = useState(0);
+    const [carrotCakeCount, setCarrotCakeCount] = useState(0);
+
+
+
+    return (  );
+}
+ 
+export default CakeContainer;
