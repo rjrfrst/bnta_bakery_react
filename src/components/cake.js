491f74f6b4864435b1604c954349b061f0b3a7ab
Rayster Ramos <rjrfrst@gmail.com>
2023-11-29 17:41:36 +0000

    tried and tested one cake, showing the react app

--- src/components/cake.js
+++ src/components/cake.js
@@ -1,5 +1,16 @@
-const Cake = () => {
-    return (  );
+// insert the destructured data
+// have to pass the props from the container 
+
+const Cake = ({cake}) => {
+    return ( 
+        <>
+        <h3>{cake.cakeName}</h3>
+        <p>ingredients: {cake.ingredients}</p>
+        <p>price: {cake.price}</p>
+        <p>rating: {cake.rating}</p>
+
+        </>
+     );
 }
  
 export default Cake;
--- src/containers/cakeContainer.js
+++ src/containers/cakeContainer.js
@@ -1,15 +1,40 @@
 import { useState } from "react";
+import Cake from "../components/cake";
 
 const CakeContainer = () => {
 
     // Containers should have states therefore: 
-    const [victoriaSpongeCount, setvictoriaSpongeCount] = useState(0);
+    const [victoriaSponge, setvictoriaSponge] = useState({
+        cakeName: "Victoria Sponge",
+           ingredients: [
+               "eggs",
+              "butter",
+              "sugar",
+              "self-raising flour",
+              "baking powder",
+              "milk"
+          ],
+            price: 5,
+            rating: 5
+    });
+
     const [teaLoafCount, setTeaLoafCount] = useState(0);
     const [carrotCakeCount, setCarrotCakeCount] = useState(0);
 
 
+    return ( 
+        <>
+        <h1>BNTA Bakery (React)</h1>
+        {/*Properties here will be stored in an object */}
+        {/*properties = {product="Victoria Sponge",  price=10, rating=5}*/} 
 
-    return (  );
+        <Cake
+            cake = {victoriaSponge}
+        />
+
+        </>
+
+     );
 }
  
 export default CakeContainer;
